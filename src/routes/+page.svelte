3933e9f20016a5aa7faf7e2f9b57bd2209da2def
Mathis Côté <77208006+BenocxX@users.noreply.github.com>
2023-02-11 11:32:38 -0500

    test: pre-hook commit with tailwind class ordering

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <h1>Welcome to SvelteKit</h1>
-<p>
+<p class="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
   Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read asd asd asd asd
   as das das dthe documentation ias das test pre-commit
 </p>

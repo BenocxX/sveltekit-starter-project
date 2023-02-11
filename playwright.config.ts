c788af4b6b745359835403919d31b996d38e2d7c
Mathis Côté <77208006+BenocxX@users.noreply.github.com>
2023-02-11 10:32:29 -0500

    refactor: prettier now uses spaces for formatting.

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,11 +1,11 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
-	webServer: {
-		command: 'npm run build && npm run preview',
-		port: 4173
-	},
-	testDir: 'src/tests/e2e/'
+  webServer: {
+    command: 'npm run build && npm run preview',
+    port: 4173
+  },
+  testDir: 'src/tests/e2e/'
 };
 
 export default config;
